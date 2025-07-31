e998af864aaeee82e8a17052db34708985021300
crazyfull <mojtaba.kahvand@gmail.com>
2025-07-31 18:02:42 +0330

    change log, debugprint...

--- src/log.h
+++ src/log.h
@@ -1,25 +1,38 @@
 #ifndef LOGING_H
 #define LOGING_H
+#include "version"
 #include <cstring>
-    #ifdef NDEBUG
-        #define DebugPrint(...) (void)0
-    #else
+#include <cstdio>
 
-        #define DEBUG_FILE_NAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
-        #define DebugPrint(...) do{ \
-        printf("<%s::%s> ", DEBUG_FILE_NAME, __func__); \
+#define DEBUG_FILE_NAME ([]() -> const char* { \
+    const char* slash = strrchr(__FILE__, '/'); \
+    const char* backslash = strrchr(__FILE__, '\\'); \
+    return slash ? slash + 1 : (backslash ? backslash + 1 : __FILE__); \
+})()
+
+#ifdef NDEBUG
+    #define DebugPrint(...) (void)0
+#else
+    #define DebugPrint(...) do { \
+        printf("\033[1;32m<%s::%s>\033[0m ", DEBUG_FILE_NAME, __func__); \
         printf(__VA_ARGS__); \
-        printf("\n");} while(0)
-    #endif
+        printf("\n"); \
+        fflush(stdout); \
+    } while(0)
+#endif
 
-
-    #ifdef NONEEDLOG
-        #define LOG(...) (void)0
-    #else
-        #define LOG(...) do{ \
+#ifdef NONEEDLOG
+    #define LOG(...) (void)0
+#else
+    #define LOG(...) do { \
         printf(__VA_ARGS__); \
-        printf("\n");} while(0);
+        printf("\n"); \
+        fflush(stdout); \
+    } while(0)
+#endif
 
+#ifndef LOG_COLOR_ENUM_DEFINED
+#define LOG_COLOR_ENUM_DEFINED
     enum Color {
         Red,
         Green,
@@ -28,25 +41,28 @@
         Black,
         White,
     };
+#endif
 
-    #define LOG_COLOR(color, ...) do{ \
-        if (color == Red)   printf("\033[31m", color); \
-        if (color == Green) printf("\033[32m", color); \
-        if (color == Yellow)printf("\033[33m", color); \
-        if (color == Blue)  printf("\033[34m", color); \
-        if (color == Black)  printf("\033[30m", color); \
-        if (color == White)  printf("\033[37m", color); \
-        printf(__VA_ARGS__); \
-    printf("\033[0m");} while(0);
+#define LOG_COLOR(color, ...) do { \
+    const char* color_code = ""; \
+    switch (color) { \
+        case Red:    color_code = "\033[31m"; break; \
+        case Green:  color_code = "\033[32m"; break; \
+        case Yellow: color_code = "\033[33m"; break; \
+        case Blue:   color_code = "\033[34m"; break; \
+        case Black:  color_code = "\033[30m"; break; \
+        case White:  color_code = "\033[37m"; break; \
+    } \
+    printf("%s", color_code); \
+    printf(__VA_ARGS__); \
+    printf("\033[0m\n"); \
+    fflush(stdout); \
+} while(0)
 
-    #endif
+#define ZERO (0)
 
-    #define ZERO (0)
-
-    #ifdef ISINVALID
-    #else
-        #define ISINVALID (-1)
-    #endif
-
+#ifndef ISINVALID
+#define ISINVALID (-1)
+#endif
 
 #endif // LOGING_H
